4365da97003ab94624b6846472df5914bd34fcf2
Logan Warren <Loganwarrenlaw@gmail.com>
2024-10-29 04:08:07 -0400

    Working website? hopefully

--- script.js
+++ script.js
@@ -2,18 +2,11 @@ const timelineWrapper = document.querySelector('.timeline-wrapper');
 const titleSection = document.querySelector('.title-section');
 const scrollArrow = document.querySelector('.scroll-arrow');
 
-timelineWrapper.addEventListener('wheel', (event) => {
-    event.preventDefault(); 
-    timelineWrapper.scrollLeft += event.deltaY * 2;
-
+timelineWrapper.addEventListener('scroll', () => {
     const scrollAmount = timelineWrapper.scrollLeft;
-    const fadeThreshold = 200; 
+    const fadeThreshold = 1; // How quickly the title and arrow fade
 
-    if (scrollAmount > fadeThreshold) {
-        titleSection.style.opacity = 0;
-        scrollArrow.style.opacity = 0;
-    } else {
-        titleSection.style.opacity = 1 - (scrollAmount / fadeThreshold);
-        scrollArrow.style.opacity = 1 - (scrollAmount / fadeThreshold);
-    }
+    // Fade out the title and arrow based on scroll amount
+    titleSection.style.opacity = 1 - Math.min(scrollAmount / fadeThreshold, 1);
+    scrollArrow.style.opacity = 1 - Math.min(scrollAmount / fadeThreshold, 1);
 });
